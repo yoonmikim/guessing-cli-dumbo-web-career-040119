b04b5e4cb88c5df91cf9b7619a4729f831ff34cd
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-28 22:02:27 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -20,9 +20,9 @@ def run_guessing_game
   input = ""
   while input
     puts "Guess a number between 1 and 6."
-    input = gets.downcase.chomp
+    input = gets.chomp
     random_number = rand(1..6).to_s
-    case input.chomp
+    case input
     when random_number
       puts "You guessed the correct number!"
     when 'exit'
